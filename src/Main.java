22b3819e6c5a71295f54c3c5269a11435e28a3c5
JoaoCzz <joaoczz0205@gmail.com>
2024-11-21 21:14:30 +0100

    Asereje

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Desde la consola");
         System.out.println("Prueba dos");
+        System.out.println("Prueba tres");
     }
 }
